cba55fece30bc6e4dc4c61b4db2d04d48114414f
Gaurav-kumarsingh <gk542998@gmail.com>
2026-05-07 23:26:09 +0530

    feat: add automated PDF extraction and classification pipeline for CS exam materials

--- scratch/fix_sync.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('public/script.js', 'utf8');
-content = content.replace(/isAnimatingPan = false;/g, 'isAnimatingPan = false;\n            syncBoardBackground();');
-fs.writeFileSync('public/script.js', content);
